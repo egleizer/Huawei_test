b562eccc55e0da2a0e66d78ca23d2979a061de1e
Jane Gleizer <elgleizer@gmail.com>
2021-01-24 20:49:45 +0300

    final

--- optimization.py
+++ optimization.py
@@ -21,35 +21,29 @@ dtype = torch.float
 device = torch.device("cpu")
 
 data = torch.normal(0, 1, size=(1, 100), device=device, dtype=dtype, requires_grad=True)
-#we can start with any point. Admittedly here as we have two local minimums where we start does matter, because we can end up in different ones
+
+#We can start with random point. Admittedly here as we have two local minimums where we start does matter, because we can end up in different ones
 s_param = torch.randn(1, 1, device=device, dtype=dtype, requires_grad=True)
-#let's use classical learning rate here
+
+#Let's use classical learning rate here
 learning_rate = 1e-6
-#the number of steps is also arbitrary here, taken from torch documentation
-for t in range(500):
+
+#The number of steps is also arbitrary here, it obtained from trial and error
+for t in range(1000):
 
     f = STESum.apply
     loss = torch.sum(f(s_param))
-
-    # To apply our Function, we use Function.apply method
-    #function = STESum.apply(s_param)
-
-    # Forward pass
-    #x.mm(w1)
-    #y_pred = function()
-
-    # Compute and print loss
-   # loss = y_pred.clone().detach()
-   # loss.requires_grad = True
-    #loss = torch.sum(function)
-
-    # Use autograd to compute the backward pass.
     loss.backward()
-    # # Update weights using gradient descent
-    #print(s_param.grad)
+    #Update weights using gradient descent
     with torch.no_grad():
         s_param -= learning_rate * s_param.grad
-        # Manually zero the gradients after updating weights
+        #Manually zero the gradients after updating weights
         s_param.grad.zero_()
 
+#Print all necessary data
+print (data)
 print (s_param)
+s_one_analytical = torch.tensordot(data, torch.sign(data)) / 100
+print (s_one_analytical)
+s_two_analytical = torch.multiply(torch.tensordot(data, torch.sign(data)) / 100, -1)
+print (s_two_analytical)
